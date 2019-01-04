7ce9965e006eab14821d414bc076aafa8d05e32a
Joey Dreijer <joey.dreijer@ing.nl>
2019-01-04 18:09:47 +0100

    Implemented tiles to download payloads via API

---
+++ src/components/PayloadTiles.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-card-group columns>
+    <b-card :header="payload.platform" v-for="payload in available_payloads">
+      <b-list-group flush>
+        <b-list-group-item v-for="type in payload.types">
+          <b-button variant="primary" :href="download_link(payload.platform, type.arch)">{{type.name}}</b-button>
+        </b-list-group-item>
+      </b-list-group>
+    </b-card>
+  </b-card-group>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "PayloadTiles",
+  data(){
+    return {
+      available_payloads: [],
+      selected_platform: "linux",
+      selected_arch: ""
+    }
+  },
+  mounted () {
+    this.get_payloads()
+  },
+  methods: {
+    get_payloads (){
+      this.$http
+        .get('payloads')
+        .then(response => this.parse_payloads(response))
+        .catch(response => this.generic_failed(response))
+    },
+    parse_payloads (response){
+      this.available_payloads = response.data;
+    },
+    download_link (platform, arch){
+      var payload_path = `${axios.defaults.baseURL}payload?platform=${platform}&arch=${arch}`;
+      return payload_path;
+   }
+  }
+};
+</script>
+
+
--- src/views/Payloads.vue
+++ src/views/Payloads.vue
@@ -1,37 +1,24 @@
 <template>
-  <div class="payloads">
+  <div class="default-payloads">
     <b-row class="top-10">
-      <b-col cols="3">
-         <b-card header="filter mitre" header-tag="header">
-    <p class="card-text">Filter tiles to only display specific agents based on hostname, username or platform</p>
-    <b-form method="get">
-      <b-row>
-        <b-col xl="3" lg="4" md="4" sm="6">
-          <b-form-radio-group id="platform-select"  button-variant="primary-reversed">
-          </b-form-radio-group>
-        </b-col>
-        <b-col xl="9" lg="8" md="8" sm="6">
-          <b-form-input type="text" placeholder="Username or Hostname"></b-form-input>
-        </b-col>
-      </b-row>
-    </b-form>
-  </b-card>
+      <b-col cols="12">
+        <PayloadTiles></PayloadTiles>
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
+import PayloadTiles from "@/components/PayloadTiles";
 
 export default {
   name: "Payloads",
   data(){
     return {
-      agent_list: [],
     }
   },
   components: {
-
+    PayloadTiles
   }
   
 };
